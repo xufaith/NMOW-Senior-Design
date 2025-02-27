1228374e91ade6e1b3f7d488be6bfc50c10805f4
prat-verma <prat7203@gmail.com>
2025-02-27 14:41:46 -0500

    website

--- inventory.session.sql
+++ inventory.session.sql
@@ -1,6 +1,9 @@
 
 USE inventory;
 
+--ingredients table--
+
+create table ingredients (
 barcode varchar(40) not null,
 ingredient_name varchar(100) not null,
 weight integer null DEFAULT 0,
@@ -12,8 +15,8 @@ category NVARCHAR(50),
 primary key (barcode)
 )engine = innodb;
 
--- ALTER TABLE Ingredients
--- DROP CONSTRAINT CK_Category;
+ALTER TABLE Ingredients
+DROP CONSTRAINT CK_Category;
 
 ALTER TABLE Ingredients
     ADD CONSTRAINT CK_Category CHECK (category IN ('meat', 'starch', 'entree', 'vegetable', 'dessert', 'appetizer', 'baking'));
@@ -21,6 +24,7 @@ ALTER TABLE Ingredients
 ALTER TABLE Ingredients
 ADD CONSTRAINT CK_Units CHECK (unit IN ('servings', 'LBS', 'count', 'cans', 'bags', 'boxes', 'sticks', 'cups', 'gallons'));
 
+--table insert examples--
 INSERT INTO Ingredients (barcode, ingredient_name, weight, quantity, unit, expiry_date, storage_location, category)
 VALUES
 ('qw_5728572', 'butter', 4, 4,  'sticks', '2025-02-24', 'J4', 'baking');
@@ -29,7 +33,6 @@ VALUES
 ('clc_4T9U25X', 'chocolate lava cake', 5,2,  'LBS', '2025-02-24', 'J4', 'entree');
 
 -- select expiry_date from nmow_database1.ingredients;
-
 create or replace view locations_update (barcode, ingredient_name, storage_location, category) as
 select barcode, ingredient_name, storage_location, category
 from ingredients
@@ -37,6 +40,8 @@ group by storage_location;
 
 USE inventory;
 
+
+--transactions table--
 CREATE TABLE transactions (
     transaction_id INT AUTO_INCREMENT PRIMARY KEY, -- Unique ID for each transaction
     barcode VARCHAR(40) NOT NULL,                 -- Link to the ingredients table via barcode
@@ -66,6 +71,7 @@ VALUES
 ('hs_5E7L23M', 'add', 4, 'Restocked 4 lbs of hoagie sandwiches'),
 ('clc_4T9U25X', 'remove', 2, 'Sold 2 lbs of chocolate lava cake to customers');
 
+
 -- quantity checks --
 SELECT ingredient_name, quantity, unit 
 FROM ingredients 
@@ -75,3 +81,27 @@ UPDATE ingredients
 SET quantity = quantity + 10 
 WHERE barcode = 'qw_5728572';
 
+DELIMITER $$
+
+CREATE TRIGGER trg_update_quantity
+AFTER INSERT ON transactions
+FOR EACH ROW
+BEGIN
+    -- Check if the transaction type is 'add'
+    IF NEW.transaction_type = 'add' THEN
+        UPDATE ingredients
+        SET quantity = quantity + NEW.quantity
+        WHERE barcode = NEW.barcode;
+
+    -- Check if the transaction type is 'remove'
+    ELSE IF NEW.transaction_type = 'remove' THEN
+        UPDATE ingredients
+        SET quantity = quantity - NEW.quantity
+        WHERE barcode = NEW.barcode;
+    END IF;
+END$$
+
+DELIMITER ;
+
+INSERT INTO transactions (barcode, transaction_type, quantity, notes)
+VALUES ('qw_5728572', 'add', 5, 'Added 5 sticks of butter for restocking');
